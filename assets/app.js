128cbb9d367f884dec6e664c008388ab61e4c764
RaeVaughan <rachelev.tx@gmail.com>
2017-05-21 18:33:07 -0500

    bootstrap html built out

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA9FkGVKS4QH7RzWOFl47tfozzLLXm4XcA",
+    authDomain: "bus-scheduler.firebaseapp.com",
+    databaseURL: "https://bus-scheduler.firebaseio.com",
+    projectId: "bus-scheduler",
+    storageBucket: "bus-scheduler.appspot.com",
+    messagingSenderId: "107320067942"
+  };
+  firebase.initializeApp(config);
